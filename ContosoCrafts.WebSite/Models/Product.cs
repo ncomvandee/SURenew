3ea9801182760002a3b9f4ff7b0b1755be8ebee7
Jara Lindsay <jlindsay@seattleu.edu>
2020-05-20 21:20:53 -0700

    Changed typos in image cards.

--- ContosoCrafts.WebSite/Models/Product.cs
+++ ContosoCrafts.WebSite/Models/Product.cs
@@ -15,10 +15,8 @@ namespace ContosoCrafts.WebSite.Models
         [JsonPropertyName("img")]
         public string Image { get; set; }
 
-        public string Url { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public int[] Ratings { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<Product>(this);
         

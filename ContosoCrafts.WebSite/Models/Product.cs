d9e6bc0513d697ff346670ea9690edfc6e39ba75
Jara Lindsay <jlindsay@seattleu.edu>
2020-05-20 21:36:26 -0700

    Took out URL and Raitings sections from products.js, and added a Date section in products.js instead.

--- ContosoCrafts.WebSite/Models/Product.cs
+++ ContosoCrafts.WebSite/Models/Product.cs
@@ -16,6 +16,9 @@ namespace ContosoCrafts.WebSite.Models
         public string Image { get; set; }
 
         public string Title { get; set; }
+
+        public string Date { get; set; }
+
         public string Description { get; set; }
 
         public override string ToString() => JsonSerializer.Serialize<Product>(this);

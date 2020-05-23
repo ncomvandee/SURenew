aae4c21958a92361a3a28632800832ec6282e69d
Nitid Comvandee <ncomvandee@seattleu.edu>
2020-05-23 02:20:03 -0700

    Added AccountModel for for storing the user account

---
+++ ContosoCrafts.WebSite/Models/AccountModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SURenew.Models
+{
+    public class AccountModel
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+
+    }
+}

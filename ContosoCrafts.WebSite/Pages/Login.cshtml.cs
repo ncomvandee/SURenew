daf5ae0ba28cf13ceb3e13329e9cabc656f949f5
Nitid Comvandee <ncomvandee@seattleu.edu>
2020-05-23 02:23:18 -0700

    Added a simple logic for site to seperate between user and administrator which will be directed to the different portal

--- ContosoCrafts.WebSite/Pages/Login.cshtml.cs
+++ ContosoCrafts.WebSite/Pages/Login.cshtml.cs
@@ -4,14 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SURenew.Models;
 
 namespace SURenew.Pages
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public AccountModel Account { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        public IActionResult OnPost()
+        {
+            if (Account.email == "jbarrons@seattleu.edu")
+            {
+                return RedirectToPage("/AdminHomePage");
+            }
+
+            return RedirectToPage("/HomePage");
         }
     }
 }

b89d8ba41f10a5c9afb5aeaa525fb857674cf09b
Nitid Comvandee <ncomvandee@seattleu.edu>
2020-05-25 16:45:22 -0700

    Added new DefaultLogin page to source control to fix a bug when clicks logout and page cannot be found

---
+++ ContosoCrafts.WebSite/Pages/DefaultLogin.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SURenew.Models;
+
+namespace SURenew.Pages
+{
+    public class DefaultLoginModel : PageModel
+    {
+        [BindProperty]
+        public AccountModel Account { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            
+            if (Account.email == "jbarrons@seattleu.edu") 
+            {
+                return RedirectToPage("/AdminHomePage");
+                
+            } else if (Account.email == "eedwards@seattleu.edu")
+            {
+                return RedirectToPage("/Homepage");
+            } else
+            {
+                return RedirectToPage("/Login");
+            }
+
+            
+        }
+    }
+}

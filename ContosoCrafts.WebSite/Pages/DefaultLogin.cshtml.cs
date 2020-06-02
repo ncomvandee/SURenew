5e8491baec105e0518b602a0e5f29efdfff329ba
Nitid Comvandee <ncomvandee@seattleu.edu>
2020-06-01 23:09:51 -0700

    Commented for function and logic in DefaultLogin.cs
    https://tasks.office.com/redhawks.onmicrosoft.com/Home/Task/HSQmeXtdskuD8XVM_YwhfGQAPbac?Type=TaskLink&Channel=Link&CreatedTime=637266749858220000

--- ContosoCrafts.WebSite/Pages/DefaultLogin.cshtml.cs
+++ ContosoCrafts.WebSite/Pages/DefaultLogin.cshtml.cs
@@ -10,6 +10,8 @@ namespace SURenew.Pages
 {
     public class DefaultLoginModel : PageModel
     {
+
+        // AccountModel property using for retrieve email and password input
         [BindProperty]
         public AccountModel Account { get; set; }
 
@@ -18,18 +20,20 @@ namespace SURenew.Pages
 
         }
 
+        // The logic function to distinguish between user and administrator
         public IActionResult OnPost()
         {
-            
+            // If login with this email(admin), site will navigate to AdminPortal
             if (Account.Email == "jbarron@seattleu.edu") 
             {
-                return RedirectToPage("/AdminPortal");
-                
+                return RedirectToPage("/AdminPortal"); 
             }
+            // If login with this email(user), site will navigate to Homepage
             if (Account.Email == "eedwards@seattleu.edu")
             {
                 return RedirectToPage("/Homepage");
-            } 
+            }
+            // Other than above valid email, the site will not allow to access
             else
             {
                 return RedirectToPage("/Login");
